ddca9daf1b40a22c21083d8820d749752cc3821e
Dhatchanamoorthi Anbazhagan <dhatcha2006@gmail.com>
2026-05-14 15:41:58 +0530

    Add files via upload

---
+++ Patterns/Pattern2.java
@@ -0,0 +1,32 @@
+/*Patterns
+    *****
+    *   *
+    *   *
+    *   *
+    *****
+*/
+
+
+
+import java.util.Scanner;
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the n : ");
+        int n = sc.nextInt();
+        int i,j;
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n;j++){
+                if(i==1 || i==n || j==1 || j==n){
+                        System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+}
