00ae60a8ff595062bfedd4a5a517163dce5fb7fd
Dhatchanamoorthi Anbazhagan <dhatcha2006@gmail.com>
2026-05-15 09:30:45 +0530

    Add files via upload

---
+++ Patterns/Pattern4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Pattern4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        int i,j,k;
+        for(i=1;i<=n;i++){
+            for(j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
