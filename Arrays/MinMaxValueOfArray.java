1111eaadcc40a6f6f0a55bc3f803ee075047aab4
Dhatchanamoorthi Anbazhagan <dhatcha2006@gmail.com>
2026-05-18 11:56:10 +0530

    Minimum and Maximum Value of the array

---
+++ Arrays/MinMaxValueOfArray.java
@@ -0,0 +1,31 @@
+// Min and Max Value of array
+
+import java.util.Scanner;
+
+public class MinMaxValueOfArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int min = arr[0];
+        for(int i=1;i<n;i++){
+            if (min>arr[i]) {
+                min=arr[i];
+            }
+        }
+        System.out.println("Min : "+min);
+
+        int max=arr[0];
+        for(int i=1;i<n;i++){
+            if (max<arr[i]) {
+                max=arr[i];
+            }
+        }
+        System.out.println("Max : "+max);
+    }
+}
