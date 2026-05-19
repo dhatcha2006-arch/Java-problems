a5f76a175376eb7f5633d3696bc0f1a00079e3d0
Dhatchanamoorthi Anbazhagan <dhatcha2006@gmail.com>
2026-05-19 13:58:36 +0530

    Constructor

---
+++ oops concepts/Constructor.java
@@ -0,0 +1,23 @@
+
+class Student 
+{
+    Student()
+    {
+        System.out.println("Hello");
+    }
+    /*
+    method
+    void get(){
+    System.out.println("Hello");
+    }
+    */
+}
+public class Constructor
+    
+{
+    public static void main(String[] args)
+    {
+        Student s=new Student();
+        //s.get();
+    }
+}
